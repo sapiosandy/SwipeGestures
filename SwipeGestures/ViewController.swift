89c22969124426ae3c8b9b537ad9d1450a9e5137
Sandy Gomez <sgomez1029@gmail.com>
2024-03-06 18:29:56 -0800

    Swipe Gestures in UIKit (UCSD Extension)

--- SwipeGestures/ViewController.swift
+++ SwipeGestures/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func leftSwipe(_ sender: UISwipeGestureRecognizer) {
+        UIView.animate(withDuration: 2.0) {
+            self.myImageView.center.x -= 100
+        }
+    }
+    @IBAction func rightSwipe(_ sender: UISwipeGestureRecognizer) {
+        UIView.animate(withDuration: 2.0) {
+            self.myImageView.center.x += 100
+        }
+    }
+    @IBAction func upSwipe(_ sender: UISwipeGestureRecognizer) {
+        self.myImageView.center.y -= 100
+    }
+    @IBAction func downSwipe(_ sender: UISwipeGestureRecognizer) {
+        self.myImageView.center.y += 100
+    }
 }
 
