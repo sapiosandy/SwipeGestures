36d5b7d26d6ad34683b6185349508f985a234917
Sandy Gomez <sgomez1029@gmail.com>
2024-03-06 18:11:30 -0800

    Initial Commit

---
+++ SwipeGestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeGestures
+//
+//  Created by Sandra Gomez on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
